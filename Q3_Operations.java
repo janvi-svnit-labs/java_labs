490b4c6f7898dae766f70e9414acf21f9a9d4077
Janvi Agravat <agravatd9@gmail.com>
2026-01-20 15:29:22 +0530

    Update Q3_Operations.java

--- Q3_Operations.java
+++ Q3_Operations.java
@@ -4,28 +4,67 @@ this program reads3 numbers and prints sum, avg, prod, largest and smallest amon
 
 import java.util.Scanner;
 
+abstract class Operations {
+    abstract void sum(int a, int b, int c);
+
+    abstract void average(int a, int b, int c);
+
+    abstract void product(int a, int b, int c);
+
+    abstract void smallest(int a, int b, int c);
+
+    abstract void largest(int a, int b, int c);
+}
+
+class Operation extends Operations {
+    public void sum(int a, int b, int c) {
+        int sum = a + b + c;
+        System.out.println("Sum: " + sum);
+    }
+
+    public void average(int a, int b, int c) {
+        double average = (a + b + c) / 3.0;
+        System.out.println("Average: " + average);
+    }
+
+    public void product(int a, int b, int c) {
+        int product = a * b * c;
+        System.out.println("Product: " + product);
+    }
+
+    public void smallest(int a, int b, int c) {
+        if (a <= b && a <= c) {
+            System.out.println(a + " is smallest");
+        } else if (b <= c && b <= a) {
+            System.out.println(b + " is smallest");
+        } else
+            System.out.println(a + " is smallest");
+    }
+
+    public void largest(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            System.out.println(a + " is largest");
+        } else if (b >= c && b >= a) {
+            System.out.println(b + " is largest");
+        } else
+            System.out.println(c + " is largest");
+    }
+}
+
 public class Q3_Operations {
-    public static void main(String args[])
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter 1st number: ");
+        Operations opt = new Operation();
+        System.out.println("Enter three numbers: ");
         int a = sc.nextInt();
-        System.out.print("Enter 2nd number: ");
         int b = sc.nextInt();
-        System.out.print("Enter 3rd number: ");
         int c = sc.nextInt();
 
-        int sum = a+b+c;
-        int avg = sum/3;
-        int prod = a*b*c;
-        int large = (a>b)?a:b;
-        int small = (a<b)?a:b;
-
-        System.out.println("Sum = "+sum);
-        System.out.println("Average = "+avg);
-        System.out.println("Product = "+prod);
-        System.out.println("Larger number = "+large);
-        System.out.println("Small = "+small);
+        opt.sum(a, b, c);
+        opt.average(a, b, c);
+        opt.product(a, b, c);
+        opt.smallest(a, b, c);
+        opt.largest(a, b, c);
 
         sc.close();
     }
